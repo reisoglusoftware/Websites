1e9dbfbee455705e72c261338bd4df2dcc5f74bf
Reisoglu <reisoglusoftware@gmail.com>
2024-02-26 18:10:37 +0300

    Tailwind commited

--- TailwindMovieWebsite/tailwind.config.js
+++ TailwindMovieWebsite/tailwind.config.js
@@ -2,7 +2,28 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    container: {
+      center: true,
+      screens: {
+        sm: "640px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1280px",
+        "2xl": "1536px",
+      },
+    },
+    extend: {
+      fontFamily: {
+        gemunu: ["Gemunu Libre", "sans-serif"],
+        kode: ["Kode Mono", "monospace"],
+      },
+      colors: {
+        "gega-red": "#BC1A45",
+        "gega-melon": "#FFD369",
+        "gega-grey": "#DDDDDD",
+        "gega-white": "#F7F7F7",
+      },
+    },
   },
   plugins: [],
 };
