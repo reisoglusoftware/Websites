3298c325d9c01793a39946c00b3f46fef131646c
Reisoglu <reisoglusoftware@gmail.com>
2024-03-05 02:44:35 +0300

    done

--- LoginPage/src/js/app.js
+++ LoginPage/src/js/app.js
@@ -6,18 +6,33 @@ const registerLink = document.querySelector(".register-link");
 const login = document.querySelector(".login");
 const closeLogin = document.querySelector(".icon-close");
 
-login.addEventListener("click", () => {
+function showLoginPage() {
+  if (wrapper.classList.contains("active")) {
+    wrapper.classList.remove("active");
+  }
+}
+
+login.addEventListener("click", (event) => {
   wrapper.classList.add("active-popup");
+  showLoginPage();
+  event.stopPropagation();
 });
 
 registerLink.addEventListener("click", () => {
   wrapper.classList.add("active");
 });
 
-loginLink.addEventListener("click", () => {
-  wrapper.classList.remove("active");
+loginLink.addEventListener("click", showLoginPage);
+
+closeLogin.addEventListener("click", (event) => {
+  wrapper.classList.remove("active-popup");
+  event.stopPropagation();
 });
 
-closeLogin.addEventListener("click", () => {
-  wrapper.classList.remove("active-popup");
+// Add event listener to the document to close the popup when clicking outside of it
+document.addEventListener("click", (event) => {
+  if (!event.target.closest(".wrapper")) {
+    // Check if the clicked element is not a child of the wrapper
+    wrapper.classList.remove("active-popup");
+  }
 });
